067ceb4c4968e6b449d89909d199db320d654093
napster-08 <97880432+napster-08@users.noreply.github.com>
2025-12-27 21:58:03 +0100

    Update binary_sensor.py

--- components/daikin_ekhhe/binary_sensor.py
+++ components/daikin_ekhhe/binary_sensor.py
@@ -36,15 +36,18 @@ async def setup_conf(config, key, hub):
         conf = config[key]
         sens = await binary_sensor.new_binary_sensor(conf)
         
-        #  INVERSION si spécifiée dans YAML
-        if CONF_INVERTED in conf and conf[CONF_INVERTED]:
+        # ✅ 'inverted' direct (pas CONF_INVERTED)
+        if conf.get('inverted', False):
             cg.add(sens.set_inverted(True))
+        
+        cg.add(hub.register_binary_sensor(key, sens))
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_EKHHE_ID])
     for key in TYPES:
 
         await setup_conf(config, key, hub)
+
 
 
 
