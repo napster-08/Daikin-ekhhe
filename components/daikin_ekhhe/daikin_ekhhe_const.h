0908cca30c10e24d63b251c86b7b889f8fb46de8
napster-08 <97880432+napster-08@users.noreply.github.com>
2025-12-27 16:28:24 +0100

    Update daikin_ekhhe_const.h

--- components/daikin_ekhhe/daikin_ekhhe_const.h
+++ components/daikin_ekhhe/daikin_ekhhe_const.h
@@ -11,13 +11,13 @@ static const std::string A_LOW_WAT_T_PROBE       = "sonde_temp_eau_basse";
 static const std::string B_UP_WAT_T_PROBE        = "sonde_temp_eau_haute";
 static const std::string C_DEFROST_T_PROBE       = "sonde_température_de_dégivrage";
 static const std::string D_SUPPLY_AIR_T_PROBE    = "sonde_temp_air_souffle";
-static const std::string E_EVA_INLET_T_PROBE     = "Sonde de temperature du gaz à l'entree de l'evaporateur";
-static const std::string F_EVA_OUTLET_T_PROBE    = "Sonde de temperature du gaz à la sortie de l'evaporateur";
-static const std::string G_COMP_GAS_T_PROBE      = "Sonde de temperature du gaz de refoulement du compresseur";
-static const std::string H_SOLAR_T_PROBE         = "Sonde de temperature pour capteur solaire";
-static const std::string I_EEV_STEP              = "Ouverture_etape_EEV";
+static const std::string E_EVA_INLET_T_PROBE     = "sonde de temperature du gaz à l'entree de l'evaporateur";
+static const std::string F_EVA_OUTLET_T_PROBE    = "sonde de temperature du gaz à la sortie de l'evaporateur";
+static const std::string G_COMP_GAS_T_PROBE      = "sonde de temperature du gaz de refoulement du compresseur";
+static const std::string H_SOLAR_T_PROBE         = "sonde de temperature pour capteur solaire";
+static const std::string I_EEV_STEP              = "ouverture_etape_EEV";
 static const std::string J_POWER_FW_VERSION      = "version_du_micrologiciel_carte_alimentation";
-static const std::string L_UI_FW_VERSION         = "Version du micrologiciel de l'interface utilisateur";
+static const std::string L_UI_FW_VERSION         = "version du micrologiciel de l'interface utilisateur";
 
 static const std::string DIG1_CONFIG             = "dig1_config";
 static const std::string DIG2_CONFIG             = "dig2_config";
@@ -33,7 +33,7 @@ static const std::string BOOST_T_TEMPERATURE     = "température_mode_boost";
 static const std::string ELECTRIC_T_TEMPERATURE  = "température_mode_electric";
 
 static const std::string P1_LOW_WAT_PROBE_HYST   = "low_water_hp_hysteris";
-static const std::string P2_HEAT_ON_DELAY        = "Retard à l'enclenchement du chauffage électrique";
+static const std::string P2_HEAT_ON_DELAY        = "retard à l'enclenchement du chauffage électrique";
 static const std::string P3_ANTL_SET_T           = "température de consigne anti-légionellose";
 static const std::string P4_ANTL_DURATION        = "durée_antilégionelle";
 static const std::string P5_DEFROST_MODE         = "mode_dégivrage";
@@ -92,4 +92,5 @@ static const std::string P52_EVA_LOW_SET         = "set_ventilateur_évaporateur
 
 }  // namespace daikin_ekhhe
 }  // namespace esphome
+
 
