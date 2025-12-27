4ca7fcea42a0a6cf11d8922d690b0af7a95b065a
napster-08 <97880432+napster-08@users.noreply.github.com>
2025-12-27 19:37:48 +0100

    Update daikin_ekhhe.h

--- components/daikin_ekhhe/daikin_ekhhe.h
+++ components/daikin_ekhhe/daikin_ekhhe.h
@@ -6,9 +6,9 @@
 #include <type_traits>
 
 #include "esphome/core/component.h"
-#include "sensor/sensor.h"
-#include "binary_sensor/binary_sensor.h"
-#include "number/number.h"
+#include "components/sensor/sensor.h"
+#include "components/binary_sensor/binary_sensor.h"
+#include "components/number/number.h"
 #include "esphome/components/select/select.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/uart/uart.h"
@@ -364,6 +364,7 @@ static const uint8_t PARAM_INDEX_INVALID = 255;
 
 }  // namespace daikin_ekkhe
 }  // namespace esphome
+
 
 
 
